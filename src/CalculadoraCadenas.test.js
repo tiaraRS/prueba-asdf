b9e7b50c6575d25096c34fe82624223d7c525070
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-10-02 00:16:56 -0200

    4F: sumar varios numeros separados por - y ,

--- src/CalculadoraCadenas.test.js
+++ src/CalculadoraCadenas.test.js
@@ -34,4 +34,10 @@ describe("Sumar", () => {
   it("deberia sumar todos los numeros separados por -", () => {
     expect(calcularCadenas("1-,2-4-8")).toEqual(1);
   });
+  it("deberia sumar todos los numeros separados por - y ,", () => {
+    expect(calcularCadenas("1-2,4,8")).toEqual(15);
+  });
+  it("deberia sumar todos los numeros separados por - y ,", () => {
+    expect(calcularCadenas("-1,2-4-,8")).toEqual(0);
+  });
 });
